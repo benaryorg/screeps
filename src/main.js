7c32e2f002e08f7dbba84abbe658dc5b72ec63ce
benaryorg <binary@benary.org>
2017-05-02 22:47:36 +0200

    harvest function
    Signed-off-by: benaryorg <binary@benary.org>

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 const HARVESTER_BODY = [WORK,CARRY,MOVE];
 
+var harvest = (function(creep)
+{
+	const sources = creep.room.find(FIND_SOURCES);
+	if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+	{
+		creep.moveTo(sources[0]);
+	}
+});
+
 const ROLE_ACTIONS =
 {
 	'harvester': (function(creep)
 	{
 		if(creep.carry.energy < creep.carryCapacity)
 		{
-			const sources = creep.room.find(FIND_SOURCES);
-			if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
-			{
-				creep.moveTo(sources[0]);
-			}
+			harvest(creep);
 		}
 		else
 		{
